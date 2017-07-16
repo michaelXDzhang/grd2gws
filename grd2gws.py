5a841a1bf9e1e6f8ed1037b9d3fb929cb1ec798a
B-Tsai <benjamin.cm.tsai@gmail.com>
2017-07-16 16:39:26 +0800

    Add meshgrid function

--- grd2gws.py
+++ grd2gws.py
@@ -7,6 +7,19 @@ Created on Sun Jul 16 15:26:55 2017
 
 import numpy as np
 
+WindSim_version = '441'
+area_name = 'No25OffshoreWindFarm'
+nxp = 600.
+nyp = 600.
+coordinate_system = '3'
+xmin = 160267.56
+xmax = 181455.29
+ymin = 2660342.39
+ymax = 2668833.35
+
+x = np.linspace(xmin,xmax,nxp)
+y = np.linspace(ymin,ymax,nyp)
+[xq,yq] = np.meshgrid(x,y);
 rawdata = np.loadtxt('grdtest.grd')
 
 
