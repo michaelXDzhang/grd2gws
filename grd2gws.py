86a4fab85aeb7a7a032dfaa6910cadea09cd02e3
B-Tsai <benjamin.cm.tsai@gmail.com>
2017-07-16 20:47:43 +0800

    Add for loop to read all .grd files

--- grd2gws.py
+++ grd2gws.py
@@ -4,26 +4,51 @@ Created on Sun Jul 16 15:26:55 2017
 
 @author: Benjamin Tsai
 """
+import os
+import glob
+import numpy as np
+from scipy.interpolate import griddata
+import matplotlib.pyplot as plt
 
 
-
+# define parameters
 WindSim_version = '441'
 area_name = 'No25OffshoreWindFarm'
-nxp = 600.
-nyp = 600.
+nxp = 600
+nyp = 600
 coordinate_system = '3'
 xmin = 160267.56
 xmax = 181455.29
 ymin = 2660342.39
 ymax = 2668833.35
 
-import numpy
-x = numpy.linspace(xmin,xmax,nxp)
-y = numpy.linspace(ymin,ymax,nyp)
-[xq,yq] = numpy.meshgrid(x,y)
-rawdata = numpy.loadtxt('grdtest.grd')
+# import data
+inpdata = np.array([0.,0.,0.])
+for filename in glob.glob('.\example\*.grd'):
+    rawdata = np.loadtxt(filename)
+    inpdata = np.vstack((inpdata, rawdata))
+inpdata = np.delete(inpdata, 0, 0)
+ximax = inpdata.max(0)
+ximin = inpdata.min(0)
+# define grid
+x = np.linspace(ximin[0],ximax[0],nxp)
+y = np.linspace(ximin[1],ximax[1],nyp)
+#x = np.linspace(xmin,xmax,nxp)
+#y = np.linspace(ymin,ymax,nyp)
+[xq,yq] = np.meshgrid(x,y)
+
+#files = glob.glob('.\example\*.grd')
+#rawdata = np.loadtxt('grdtest.grd')
 points = rawdata[:,0:2].copy()
 values = rawdata[:,2].copy()
 
-from scipy.interpolate import griddata
+# grid the data
 zq = griddata(points, values, (xq, yq), method='nearest')
+
+CS = plt.contour(xq, yq, zq, 5, linewidths=0.5, colors='k')
+CS = plt.contourf(xq, yq, zq, 5,
+                  vmax=abs(zq).max(), vmin=-abs(zq).max())
+plt.colorbar()  # draw colorbar
+plt.scatter(points[:,0], points[:,1], marker='o', s=5, zorder=10)
+plt.xlim(xmin, xmax)
+plt.ylim(ymin, ymax)
