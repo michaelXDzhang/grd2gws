5e669ad7c2f080bfdedb693973feb3bbe1aad332
B-Tsai <benjamin.cm.tsai@gmail.com>
2017-07-16 17:12:47 +0800

    Add griddata function

--- grd2gws.py
+++ grd2gws.py
@@ -5,7 +5,7 @@ Created on Sun Jul 16 15:26:55 2017
 @author: Benjamin Tsai
 """
 
-import numpy as np
+
 
 WindSim_version = '441'
 area_name = 'No25OffshoreWindFarm'
@@ -17,10 +17,13 @@ xmax = 181455.29
 ymin = 2660342.39
 ymax = 2668833.35
 
-x = np.linspace(xmin,xmax,nxp)
-y = np.linspace(ymin,ymax,nyp)
-[xq,yq] = np.meshgrid(x,y);
-rawdata = np.loadtxt('grdtest.grd')
+import numpy
+x = numpy.linspace(xmin,xmax,nxp)
+y = numpy.linspace(ymin,ymax,nyp)
+[xq,yq] = numpy.meshgrid(x,y)
+rawdata = numpy.loadtxt('grdtest.grd')
+points = rawdata[:,0:2].copy()
+values = rawdata[:,2].copy()
 
-
-
+from scipy.interpolate import griddata
+zq = griddata(points, values, (xq, yq), method='nearest')
